dd86e90d0c8f31f85ebeb8c6bfda4968e445df0e
rbjrichy <rbjrichy@gmail.com>
2023-04-27 22:13:10 -0400

    correccion seed

--- database/seeders/DatosSeeder.php
+++ database/seeders/DatosSeeder.php
@@ -15,43 +15,43 @@ class DatosSeeder extends Seeder
     public function run()
     {
 
-        // $sexos = ['Macho', 'Hembra'];
-        // foreach ($sexos as $sexo) {
-        //     Dato::create([
-        //         'tipo' => 'sexo_mascota',
-        //         'valor' => $sexo,
-        //     ]);
-        // }
+        $sexos = ['Macho', 'Hembra'];
+        foreach ($sexos as $sexo) {
+            Dato::create([
+                'tipo' => 'sexo_mascota',
+                'valor' => $sexo,
+            ]);
+        }
 
-        // $meses = ['Ninguno', 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-        // foreach ($meses as $mes) {
-        //     Dato::create([
-        //         'tipo' => 'meses',
-        //         'valor' => $mes,
-        //     ]);
-        // }
+        $meses = ['Ninguno', 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        foreach ($meses as $mes) {
+            Dato::create([
+                'tipo' => 'meses',
+                'valor' => $mes,
+            ]);
+        }
 
-        // $especies = ['Canino', 'Gatuno', 'Avicola', 'Porcino', 'Vacuno', 'Equino', 'Piscicola'];
-        // foreach ($especies as $especie) {
-        //     Dato::create([
-        //         'tipo' => 'especie',
-        //         'valor' => $especie,
-        //     ]);
-        // }
-        // $eventos = ['Simposios', 'Ferias', 'Adopciones', 'Voluntariado'];
-        // foreach ($eventos as $evento) {
-        //     Dato::create([
-        //         'tipo' => 'eventos',
-        //         'valor' => $evento,
-        //     ]);
-        // }
-        // $departamentos = ['Santa Cruz', 'La Paz', 'Cochabamba', 'Chuquisaca', 'Tarija', 'Potosi', 'Oruro', 'Beni', 'Pando'];
-        // foreach ($departamentos as $deparamento) {
-        //     Dato::create([
-        //         'tipo' => 'departamentos',
-        //         'valor' => $deparamento,
-        //     ]);
-        // }
+        $especies = ['Canino', 'Gatuno', 'Avicola', 'Porcino', 'Vacuno', 'Equino', 'Piscicola'];
+        foreach ($especies as $especie) {
+            Dato::create([
+                'tipo' => 'especie',
+                'valor' => $especie,
+            ]);
+        }
+        $eventos = ['Simposios', 'Ferias', 'Adopciones', 'Voluntariado'];
+        foreach ($eventos as $evento) {
+            Dato::create([
+                'tipo' => 'eventos',
+                'valor' => $evento,
+            ]);
+        }
+        $departamentos = ['Santa Cruz', 'La Paz', 'Cochabamba', 'Chuquisaca', 'Tarija', 'Potosi', 'Oruro', 'Beni', 'Pando'];
+        foreach ($departamentos as $deparamento) {
+            Dato::create([
+                'tipo' => 'departamentos',
+                'valor' => $deparamento,
+            ]);
+        }
         $seccion_img = ['header', 'footer','body'];
         foreach ($seccion_img as $tipo) {
             Dato::create([
