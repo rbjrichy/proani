aaf4adab6ff2f03079edabee900cad1814098ead
rbjrichy <rbjrichy@gmail.com>
2023-08-18 17:04:14 -0400

    carrusel quienes somos

--- resources/views/theme/proanisrl/pages/quienes_somos.blade.php
+++ resources/views/theme/proanisrl/pages/quienes_somos.blade.php
@@ -1,9 +1,10 @@
 @extends('theme.proanisrl.master')
 
 @section('title', 'Quienes Somos')
-{{-- @section('classes_body')
-    {{'bg-patitas'}}
-@endsection --}}
+@section('custom_css')
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+@stop
 @section('body')
 {{-- menu flotante --}}
 @php
@@ -12,7 +13,7 @@ $menu = $menuComponent->menu;
 @endphp
 @include('theme.proanisrl.partials.nav.menu-flot')
 <div class="contenedor ">
-    <div class="bg-nosotros">
+    <div class="bg-nosotros" style="" >
       <div class="contenedor-nosotoros">
         <div class="proani-letras">
           <div class="proani-texto">
@@ -33,7 +34,7 @@ $menu = $menuComponent->menu;
                 <div class="linea-hijo-top"></div>
                 <div class="linea-hijo-top"></div>
               </div>
-              <p>A su retorno de Europa y luego de soñar durante años con un emprendimiento dedicado a cuidad la salud y bienestar de los animales,
+              <p>A su retorno de Europa y luego de soñar durante años con un emprendimiento dedicado a cuidar la salud y bienestar de los animales,
                 Alexsis y Raúl Serrano inauguran Lassie, la primer veterinaria de Santa Cruz.
               </p>
               <div class="cuadro-padre">
@@ -111,7 +112,7 @@ $menu = $menuComponent->menu;
           </div>
           <h2><span>NUESTRO</span>MANIFIESTO</h2>
           <P>
-            Es la expresión fundamental de nuestra convicción más profunda, aquella cerdad irrefutable que fuimos descubriendo en el camino
+            Es la expresión fundamental de nuestra convicción más profunda, aquella verdad irrefutable que fuimos descubriendo en el camino
             y que da vida a todo lo que hacemos.
           </P>
           <p><span>¿QUIÉN DIJO QUE SOLO QUIENES TIENEN TU SANGRE SON TU FAMILIA?</span></p>
@@ -125,7 +126,7 @@ $menu = $menuComponent->menu;
           <p>Todos los negocios tenemos un fin último que va más allá de los réditos econonómicos: el binestar humano.</p>
           <p>
             Nuestra Misión de Transformación Masiva establece el CÓMO buscamos contribuir a ese biniestar colectivo.
-            Ese CÓMO es una combinación de lo que nos gurata hacer, de lo que somos buenos haciendo, de lo que el mundo necesita
+            Ese CÓMO es una combinación de lo que nos gusta hacer, de lo que somos buenos haciendo, de lo que el mundo necesita
             y por lo que la sociedad está dispuesta a pagar.
           </p>
           <h2><span>NUESTRO</span>NEGOCIO</h2>
@@ -142,6 +143,27 @@ $menu = $menuComponent->menu;
 
         </div>
       </div>
+      <div class="carrusel-contenedor">
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <div class="carousel-item">
+              <img src="{{asset('theme/proanisrl/img/carrusel/perro1.png')}}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item active">
+                <img src="{{asset('theme/proanisrl/img/carrusel/gato1.png')}}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="{{asset('theme/proanisrl/img/carrusel/perro2.png')}}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="{{asset('theme/proanisrl/img/carrusel/gato2.png')}}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+              <img src="{{asset('theme/proanisrl/img/carrusel/perro3.png')}}" class="d-block w-100" alt="...">
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </div>
 
@@ -154,5 +176,8 @@ $menu = $menuComponent->menu;
 <!-- End footer -->
 @endsection
 @section('custom_js')
-
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 @endsection
